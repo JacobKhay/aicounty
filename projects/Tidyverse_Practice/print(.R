6f0916b5b04d0b61a4df2a860a0d5a60ac760324
Jacob Khaykin <jacobkhaykin@icloud.com>
2025-07-20 14:52:52 -0400

    Remove unwanted files/folders and update .gitignore

--- projects/Tidyverse_Practice/print(.R
+++
@@ -1,9 +0,0 @@
-
-
-ggplot(economics, aes(x = psavert)) +
-  geom_histogram(bins = 30, fill = "darkgreen", color = "white") +
-  labs(
-    title = "Histogram of Personal Savings Rate",
-    x = "Personal Savings Rate (%)",
-    y = "Number of Months"
-  )
